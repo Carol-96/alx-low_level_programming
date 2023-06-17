d7b0dd2de9b14618c9a7031ea58b02dd9aea2b88
Carol-96 <karolynkalumu@gmail.com>
2023-06-17 07:25:12 -0700

    lowercase only

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+
+/**
+ * main - Prints the alphabets without q and e
+ *
+ * Return: Always (Success)
+ */
+int main(void)
+{
+	char b;
+
+	for (b = 'a'; b <= 'z'; b++)
+	{
+		if (b == 'q' || b == 'e')
+		{
+			continue;
+		}
+		putchar(b);
+	}	
+	putchar('\n');
+	return (0);
+}
+
